e18a425fdc3624013181354a8db76782a0242a6a
Paras Rawat <drago138150@gmail.com>
2024-10-19 20:47:20 +0530

    Progress Component Bug Resolution

--- client/src/components/ui/progress.tsx
+++ client/src/components/ui/progress.tsx
@@ -3,22 +3,28 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+interface customProgressComponentProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  progressBarColor?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  // React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  customProgressComponentProps
 >(({ className, value, progressBarColor, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
       "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-      className
+      className,
     )}
     {...props}
   >
     {/* <span>{progressBarColor}</span> */}
     <ProgressPrimitive.Indicator
       // bg-primary
-      className={cn("h-full w-full flex-1  transition-all")}
+      className={cn("h-full w-full flex-1 transition-all")}
       style={{
         transform: `translateX(-${100 - (value || 0)}%)`,
         backgroundColor: progressBarColor,
